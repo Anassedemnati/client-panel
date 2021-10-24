c9fc778c11514513487b99a7016f489ec65af930
Anasse Demnati <demnati_anasse@emsi-edu.ma>
2021-10-24 19:36:04 +0200

    Params in Router

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
               <Switch>
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
-                <Route exact path="/about" component={About} />
+                <Route exact path="/about/:id/:name" component={About} />
                 <Route component={PageNotFound} />
               </Switch>
                    
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function About() {
+export default function About(props) {
     return (
         <div>
             <h2>About page</h2>
+            <h5>{props.match.params.id}</h5>
+            
+            <h5>{props.match.params.name}</h5>
             <p>
                 
             </p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt doloribus fugiat enim sint iusto facere illo tempora, veritatis provident distinctio.
